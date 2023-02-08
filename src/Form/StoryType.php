fd24fd605f324aa5760acc1b241639d51db0b4d8
Mai Julie <maijulie@MBP-de-Mai.lan>
2023-02-08 13:50:13 +0100

    ajout

--- src/Form/StoryType.php
+++ src/Form/StoryType.php
@@ -2,23 +2,23 @@
 
 namespace App\Form;
 
+use App\Entity\Story;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactType extends AbstractType
+class StoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Story::class,
         ]);
     }
 }
